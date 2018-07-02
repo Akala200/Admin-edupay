5347de66d17904f041a93fcf5913e9df0c146787
adeniyi akala <adeiyiakala91@gmail.com>
2018-07-02 11:55:19 +0100

    13th commit

--- src/app/theme/components/sidebar/sidebar.component.ts
+++ src/app/theme/components/sidebar/sidebar.component.ts
@@ -10,35 +10,35 @@ import { MenuService } from '../menu/menu.service';
   encapsulation: ViewEncapsulation.None,
   providers: [ MenuService ]
 })
-export class SidebarComponent implements OnInit {  
+export class SidebarComponent implements OnInit {
   public settings: Settings;
-  public menuItems:Array<any>;
-  constructor(public appSettings:AppSettings, public menuService:MenuService) {
+  public menuItems: Array<any>;
+  constructor(public appSettings: AppSettings, public menuService: MenuService) {
       this.settings = this.appSettings.settings;
       this.menuItems = this.menuService.getVerticalMenuItems();
   }
 
-  ngOnInit() {     
-    if(sessionStorage["userMenuItems"]) {
-      let ids = JSON.parse(sessionStorage.getItem("userMenuItems"));
-      let newArr = [];
+  ngOnInit() {
+    if (sessionStorage['userMenuItems']) {
+      const ids = JSON.parse(sessionStorage.getItem('userMenuItems'));
+      const newArr = [];
       ids.forEach(id => {
-        let newMenuItem = this.menuItems.filter(mail => mail.id == id);
+        const newMenuItem = this.menuItems.filter(mail => mail.id === id);
         newArr.push(newMenuItem[0]);
       });
-      this.menuItems = newArr; 
+      this.menuItems = newArr;
     }
   }
 
-  public closeSubMenus(){
-    let menu = document.querySelector("#menu0");
+  public closeSubMenus() {
+    const menu = document.querySelector('#menu0');
     for (let i = 0; i < menu.children.length; i++) {
-        let child = menu.children[i].children[1]; 
-        if(child){
-            if(child.classList.contains('show')){
+        const child = menu.children[i].children[1];
+        if (child) {
+            if (child.classList.contains('show')) {
               child.classList.remove('show');
-              menu.children[i].children[0].classList.add('collapsed'); 
-            }             
+              menu.children[i].children[0].classList.add('collapsed');
+            }
         }
     }
   }
